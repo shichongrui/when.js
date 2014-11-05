274768d3b8977bbd448bdd62b87231fb42779bc2
Matthew Sessions <shichongrui@gmail.com>
2014-11-05 13:40:11 -0700

    Somehow the original code got minified. Got it back

--- when.js
+++ when.js
@@ -1,1 +1,34 @@
-var when=function(){var n=[],t=[];return function(r,e){void 0!==window[r]?e():(n[r]=n[r]||[],n[r].push(e),window.hasOwnProperty(r)||Object.defineProperty(window,r,{get:function(){return t[r]},set:function(e){t[r]=e;for(var o=n[r].length,i=0;o>i;i++)n[r][i].call(this);n[r]=null}}))}}();
+var when = (function () {
+    var callbacks = [],
+        whenObjects = [];
+    
+    return function (object, callback) {
+        //if the variable already exists then we should just run the function
+        if (window[object] !== undefined) {
+            callback();
+        } else {
+            callbacks[object] = callbacks[object] || [];
+            callbacks[object].push(callback);
+
+            if (!window.hasOwnProperty(object)) {
+                //put a getter and setter on the window object
+                Object.defineProperty(window, object, {
+                    get: function () {
+                        return whenObjects[object];
+                    },
+                    set: function (variable) {
+                        whenObjects[object] = variable;
+                        //call all functions 
+                        var numCallbacks = callbacks[object].length;
+                        for (var i = 0; i < numCallbacks; i++) {
+                            callbacks[object][i].call(this);
+                        }
+                        //after all of the callbacks have been run it will remove all of the callbacks so that
+                        //we can free up some memory
+                        callbacks[object] = null;
+                    }
+                });
+            }
+        }
+    }
+})();
